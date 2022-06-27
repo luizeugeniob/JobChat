8a54f17c2d9eb3a65a8d89f680f341c5d96c4d42
Luiz Eugenio Barbieri <luizeugeniobarbieri@gmail.com>
2022-06-26 23:13:41 -0300

    Adding login with Identity.

---
+++ JobChat.Infra/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using JobChat.Domain.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace JobChat.Infra.Contexts
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
